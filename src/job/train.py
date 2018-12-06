299d387d3f6a9c1ecaf6e8ef6ae420eae4f6ce23
Lia <liabifano@gmail.com>
2018-12-06 15:43:04 +0100

    Update train.py

--- src/job/train.py
+++ src/job/train.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
 
     try:
         s.logging.info('Starting {file}'.format(file=FILENAME))
+        
         inputs = h.read_inputs(s.INPUT_PREFIX, INPUTS_FILES)
         outputs = task(*inputs)
         h.save_outputs(s.OUTPUT_PREFIX, outputs, OUTPUTS_FILES, FILENAME)
