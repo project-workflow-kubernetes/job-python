8875157a957cfd23431c3a6473b71e25a0464537
Lia <liabifano@gmail.com>
2018-12-07 10:00:06 +0100

    Update train.py

--- src/job/train.py
+++ src/job/train.py
@@ -20,7 +20,6 @@ def task(X_train, X_val, y_train, y_val):
     data dependencies: `X_train`, `X_val`, `y_train`, `y_val`
     data outputs: `trained_model`
     '''
-    
     clf = MLPClassifier(solver='adam', hidden_layer_sizes=350, alpha=1e-03)
     clf.fit(X_train, y_train)
     score = clf.score(X_val, y_val)
